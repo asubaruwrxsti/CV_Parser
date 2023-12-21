b626fac858dbafcde7533d91850e09057e675897
Arlind Ismalaja <60890408+asubaruwrxsti@users.noreply.github.com>
2023-12-21 10:23:21 +0100

    Refactored Indetation

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ client = OpenAI(
 
 try:
     completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-    ]
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+            {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+        ]
     )
 except Exception as e:
     print(f'Failed: \n{e}')
