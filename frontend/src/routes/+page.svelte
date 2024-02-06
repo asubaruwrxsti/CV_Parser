e5ee028d1787252593d7c060e79e08fdf6a43303
asubaruwrxsti <arlind12344@gmail.com>
2024-02-06 13:21:48 +0100

    Added small transition

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { onMount } from "svelte";
+	import { fade } from "svelte/transition";
 
 	let isLoading = true;
+	let showButtons = false;
 
 	onMount(() => {
 		setTimeout(() => {
@@ -24,23 +26,24 @@
 					class="w-full h-full"
 				/>
 			</div>
-			<div
-				class="h-full w-1/3 bg-black text-white p-8 flex flex-col items-center justify-center"
+            <div
+                class="h-full w-1/3 bg-black text-white p-8 flex flex-col items-center justify-center"
+				in:fade={{ duration: 500 }}
 			>
-				<div class="mb-4">
-					<h2 class="text-4xl">Welcome</h2>
-				</div>
-				<div class="flex flex-row items-center justify-center w-full">
-					<button
-						class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-10 rounded text-xl mr-4 transform transition duration-500 ease-in-out"
-						>Login</button
-					>
-					<button
-						class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-10 rounded text-xl transform transition duration-500 ease-in-out"
-						>Signup</button
-					>
-				</div>
-			</div>
+                <div class="mb-4">
+                    <h2 class="text-4xl">Welcome</h2>
+                </div>
+                <div class="flex flex-row items-center justify-center w-full">
+                    <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-10 rounded text-xl mr-4 transform transition duration-500 ease-in-out"
+                        >Login</button
+                    >
+                    <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-10 rounded text-xl transform transition duration-500 ease-in-out"
+                        >Signup</button
+                    >
+                </div>
+            </div>
 		</div>
 	{/if}
 </div>
