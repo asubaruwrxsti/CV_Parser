c829d338ad23431be43d9f8aa5b5e513cfd7d306
Arlind <arlind12344@gmail.com>
2024-02-16 12:48:14 +0100

    Set up slug project page

--- frontend/src/routes/projects/[id]/+page.svelte
+++ frontend/src/routes/projects/[id]/+page.svelte
@@ -2,25 +2,53 @@
 	/** @type {import('./$types').PageData} */
 	export let data: any;
 	import { onMount } from "svelte";
-	
-	let projects: any = [];
+	import { browser } from "$app/environment";
+	import { checkSession } from "../../../services/sessionManager";
+
+	let isLoading = true;
+	let project: any = [];
 
 	onMount(async () => {
 		console.log(data.props.data.id);
 
 		try {
-			let projectUrl = "http://localhost:8000/projects/" + data.props.data.id;
+			let projectUrl =
+				"http://localhost:8000/projects/" + data.props.data.id;
 			let response = await fetch(projectUrl, {
 				method: "GET",
 				headers: {
 					Authorization: `Bearer ${localStorage.getItem("session")}`,
 				},
 			});
-			projects = await response.json().then((data) => {
+			project = await response.json().then((data) => {
 				console.log(data);
 			});
 		} catch (error) {
 			console.log(error);
 		}
 	});
+
+	if (browser) {
+		async () => {
+			const sessionValid = await checkSession();
+			if (sessionValid) {
+				window.location.href = "/dashboard";
+			} else {
+				isLoading = false;
+			}
+		};
+
+		setTimeout(() => {
+			isLoading = false;
+		}, 500);
+	}
 </script>
+
+{#if isLoading}
+	<div class="flex items-center justify-center min-h-screen transition-main">
+		<div
+			class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500"
+		></div>
+	</div>
+{:else}
+{/if}
