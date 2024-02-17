37382d36c07915faef5e11a61969a6f9c62548b6
asubaruwrxsti <arlind12344@gmail.com>
2024-02-17 15:56:28 +0100

    Added participant search endpoint

--- app/routers/participants.py
+++ app/routers/participants.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Request, HTTPException, Depends
-from app.models.models import Project, Applicants
+from fastapi import APIRouter, Request, Depends
+from app.models.models import Applicants
 from app.dependencies import Database, get_current_user
 from app.services import service
 
@@ -9,3 +9,10 @@ applicants = Applicants()
 @router.get("/")
 async def read():    
     return await applicants.get_records(Database())
+
+@router.post("/search")
+async def search(request: Request):
+    fields = await request.json()
+    query = "SELECT * FROM applicants WHERE " + " AND ".join([f"CAST(applicants.{key} AS TEXT) ILIKE '%{value}%' LIMIT 3" for key, value in fields.items()])
+
+    return Database().query(query)
