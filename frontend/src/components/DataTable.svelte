83e0bfb003ac38a486a342d0ee55d4e5489c5386
Arlind <arlind12344@gmail.com>
2024-03-01 17:07:07 +0100

    Minor improvements

--- frontend/src/components/DataTable.svelte
+++ frontend/src/components/DataTable.svelte
@@ -31,11 +31,11 @@
 	let tableAttrs: any;
 	let tableBodyAttrs: any;
 	let pluginStates: any;
-	let filterValue: string;
+	let filterValue: any;
 
-	let hasNextPage: boolean;
-	let hasPreviousPage: boolean;
-	let pageIndex: number;
+	let hasNextPage: any;
+	let hasPreviousPage: any;
+	let pageIndex: any;
 
 	async function loadData() {
 		data = await fetchData(property);
@@ -74,88 +74,96 @@
 </script>
 
 {#if headerRows && pageRows}
-	<div class="flex items-center py-4">
-		<Input
-			class="max-w-sm"
-			placeholder="Filter emails..."
-			type="text"
-			bind:value={$filterValue}
-		/>
-	</div>
-	<div class="rounded-md border">
-		<Table.Root {...$tableAttrs}>
-			<Table.Header>
-				{#each $headerRows as headerRow}
-					<Subscribe rowAttrs={headerRow.attrs()}>
-						<Table.Row>
-							{#each headerRow.cells as cell (cell.id)}
-								<Subscribe
-									attrs={cell.attrs()}
-									let:attrs
-									props={cell.props()}
-									let:props
-								>
-									<Table.Head {...attrs}>
-										{#if cell.id === " "}
-											<div class="text-right">
+	<div>
+		<div class="flex items-center py-4">
+			<Input
+				class="max-w-sm"
+				placeholder="Filter emails..."
+				type="text"
+				bind:value={$filterValue}
+			/>
+		</div>
+		<div class="rounded-md border">
+			<Table.Root {...$tableAttrs}>
+				<Table.Header>
+					{#each $headerRows as headerRow}
+						<Subscribe rowAttrs={headerRow.attrs()}>
+							<Table.Row>
+								{#each headerRow.cells as cell (cell.id)}
+									<Subscribe
+										attrs={cell.attrs()}
+										let:attrs
+										props={cell.props()}
+										let:props
+									>
+										<Table.Head {...attrs}>
+											{#if cell.id === " "}
+												<div class="text-right">
+													<Render
+														of={cell.render()}
+													/>
+												</div>
+											{:else if cell.id === "name"}
+												<Button
+													variant="ghost"
+													on:click={props.sort.toggle}
+												>
+													<Render
+														of={cell.render()}
+													/>
+													<ArrowUpDown
+														class={"ml-2 h-4 w-4"}
+													/>
+												</Button>
+											{:else}
 												<Render of={cell.render()} />
-											</div>
-										{:else if cell.id === "name"}
-											<Button
-												variant="ghost"
-												on:click={props.sort.toggle}
-											>
+											{/if}
+										</Table.Head>
+									</Subscribe>
+								{/each}
+							</Table.Row>
+						</Subscribe>
+					{/each}
+				</Table.Header>
+				<Table.Body {...$tableBodyAttrs}>
+					{#each $pageRows as row (row.id)}
+						<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
+							<Table.Row>
+								{#each row.cells as cell (cell.id)}
+									<Subscribe attrs={cell.attrs()} let:attrs>
+										<Table.Cell>
+											{#if cell.id === " "}
+												<div class="text-right">
+													<Render
+														of={cell.render()}
+													/>
+												</div>
+											{:else}
 												<Render of={cell.render()} />
-												<ArrowUpDown
-													class={"ml-2 h-4 w-4"}
-												/>
-											</Button>
-										{:else}
-											<Render of={cell.render()} />
-										{/if}
-									</Table.Head>
-								</Subscribe>
-							{/each}
-						</Table.Row>
-					</Subscribe>
-				{/each}
-			</Table.Header>
-			<Table.Body {...$tableBodyAttrs}>
-				{#each $pageRows as row (row.id)}
-					<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-						<Table.Row>
-							{#each row.cells as cell (cell.id)}
-								<Subscribe attrs={cell.attrs()} let:attrs>
-									<Table.Cell>
-										{#if cell.id === " "}
-											<div class="text-right">
-												<Render of={cell.render()} />
-											</div>
-										{:else}
-											<Render of={cell.render()} />
-										{/if}
-									</Table.Cell>
-								</Subscribe>
-							{/each}
-						</Table.Row>
-					</Subscribe>
-				{/each}
-			</Table.Body>
-		</Table.Root>
-	</div>
-	<div class="flex items-center justify-end space-x-4 py-4">
-		<Button
-			variant="outline"
-			size="sm"
-			on:click={() => ($pageIndex = $pageIndex - 1)}
-			disabled={!$hasPreviousPage}>Previous</Button
-		>
-		<Button
-			variant="outline"
-			size="sm"
-			disabled={!$hasNextPage}
-			on:click={() => ($pageIndex = $pageIndex + 1)}>Next</Button
-		>
+											{/if}
+										</Table.Cell>
+									</Subscribe>
+								{/each}
+							</Table.Row>
+						</Subscribe>
+					{/each}
+				</Table.Body>
+			</Table.Root>
+		</div>
+		<div class="flex items-center justify-end space-x-4 py-4">
+			<Button
+				variant="outline"
+				size="sm"
+				on:click={() => ($pageIndex = $pageIndex - 1)}
+				disabled={!$hasPreviousPage}>Previous</Button
+			>
+			<Button
+				variant="outline"
+				size="sm"
+				disabled={!$hasNextPage}
+				on:click={() => ($pageIndex = $pageIndex + 1)}>Next</Button
+			>
+		</div>
 	</div>
 {:else}
 	<p>Loading...</p>
